0daa4c0f350ad339fdd85212fa8aa11d7d512656
Brazenbillygoat <hyrum.butler3@gmail.com>
2020-09-16 15:17:49 -0400

    built new and creates views and controller action with a reroute from create back to the show

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -13,7 +13,12 @@ class CouponsController < ApplicationController
     end
 
     def create
+        new_store = params["coupon"]["store"]
+        new_coupon_code = params["coupon"]["coupon_code"]
 
+        new_coupon = Coupon.create(store: new_store, coupon_code: new_coupon_code)
+        
+        redirect_to coupon_path(new_coupon)
     end
 
     def new
